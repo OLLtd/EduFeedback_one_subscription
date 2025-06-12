b1e235ad1a587e1707bb5eb0d418e7467a452804
Harleen Sharma <har_leen123@yahoo.com>
2025-06-12 12:52:02 +0530

    Services Page Changes
    Services Page Changes

---
+++ EduFeedback.Service/Models/CourseSubscriptionViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EduFeedback.Service.Models
+{
+    public class CourseSubscriptionViewModel
+    {
+        public int Year { get; set; }
+        public int Subject { get; set; }
+        public int AssignmentsPerWeek { get; set; }
+        public string PromoCode { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Captcha { get; set; }
+        
+        public IEnumerable<SelectListItem> SchoolYearList { get; set; }
+
+        public IEnumerable<SelectListItem> SubjectList { get; set; }
+    }
+
+   
+}
---
+++ EduFeedback.Web/Controllers/ServicesController.cs
@@ -0,0 +1,52 @@
+﻿using EduFeedback.Service.Models;
+using EduFeedback.Service.Services;
+using EduFeedback.Web.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace EduFeedback.Web.Controllers
+{
+
+    public class ServicesController : Controller
+    {
+
+        public ActionResult OneSubscription()
+        {
+            var userService = new RegistrationService();
+            var schoolYearList = userService.GetSchoolYearlist().Select(c => new SelectListItem
+            {
+                Value = c.Year_ID.ToString(),
+                Text = c.YearName.ToString()
+            });
+
+            var subjectList = userService.SubjectList().Select(c => new SelectListItem
+            {
+                Value = c.ID.ToString(),
+                Text = c.SubjectName.ToString()
+            });
+
+            var model = new CourseSubscriptionViewModel
+            {
+                
+                SubjectList = subjectList,
+                SchoolYearList = schoolYearList,
+               
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult OneSubscription(CourseSubscriptionViewModel model)
+        {
+
+            return View(model);
+
+        }
+    }
+}
